8dd8c6a4b7a1766641f3253d77d6245cb9aa9873
isakashiori <isslamdunk0000@gmail.com>
2024-06-16 20:37:33 +0900

    desktopからsidekiqの管理画面を確認できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  require 'sidekiq-scheduler/web'
+
+  Sidekiq::Web.use(Rack::Auth::Basic) do |user_id, password|
+    [user_id, password] == [ENV['USER_ID'], ENV['USER_PASSWORD']]
+  end
+  mount Sidekiq::Web, at: '/sidekiq'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
