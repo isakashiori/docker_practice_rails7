e7c775460010f055bc7b28a60e7227c1d34ca46f
isakashiori <isslamdunk0000@gmail.com>
2024-07-04 22:09:32 +0900

    wip:　NARUTOの名言をlinebotで送信できるように実装中

---
+++ app/services/line_bot_service.rb
@@ -0,0 +1,23 @@
+class LineBotService
+  def initialize(random_word)
+    @random_word = random_word
+  end
+
+  def perform
+    user_id = "lineのユーザーID"
+    message= {
+      type: 'text',
+      text: @random_word
+    }
+    client.push_message(user_id, message)
+  end
+
+  private
+
+  def client  
+    @client ||= Line::Bot::Client.new { |config|  
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]  
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]  
+    }  
+  end
+end
