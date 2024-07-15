95611ab7c618d170d71a1f9535bcc9b12e607565
isakashiori <isslamdunk0000@gmail.com>
2024-07-15 16:54:03 +0900

    fix: タイポを修正

--- app/services/gpt_reply_service.rb
+++ app/services/gpt_reply_service.rb
@@ -1,5 +1,9 @@
-class GetReplyService
-  def perform(ask_msg)
+class GptReplyService
+  def initialize(ask_msg)
+    @ask_msg = ask_msg
+  end
+
+  def perform
     # 一旦エンドポイントは以下しか使わない予定で書いている
     # より汎用的に書くのであればFaraday.newでコネクションのインスタンスを作成した方がいい
     response = Faraday.post("https://api.openai.com/v1/chat/completions") do |req|
@@ -7,7 +11,7 @@ class GetReplyService
       req.headers["Authorization"] = "Bearer #{ENV['OPENAI_API_KEY']}"
       req.body = {
                     model: "gpt-3.5-turbo",
-                    messages: [{role: "user", content: ask_msg}],
+                    messages: [{role: "user", content: @ask_msg}],
                     temperature: 0.7
                   }.to_json
     end
