cd3d19df5cd94386b1b6e7bbc608cc2618f174ab
isakashiori <isslamdunk0000@gmail.com>
2024-07-15 20:29:16 +0900

    change: CSRF保護を無効にして外部クライアントからのリクエストを許可

--- app/controllers/gpt_reply_messages_controller.rb
+++ app/controllers/gpt_reply_messages_controller.rb
@@ -1,4 +1,4 @@
-class GptReplyMessagesController < ApplicationController
+class GptReplyMessagesController < ActionController::API
   def create
     # faradayでapiを叩くコードが長くなるため、servicesファイルに切り出す
     response = GptReplyService.new(params[:ask_msg]).perform
