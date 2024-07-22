f52aa64b4aba2a92cc1ba662763f323f25e2fdbc
isakashiori <isslamdunk0000@gmail.com>
2024-07-22 11:08:28 +0900

    feat: schemaファイルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_03_010354) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_22_020336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_03_010354) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "statuses", force: :cascade do |t|
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_statuses_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.text "image_data"
@@ -27,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_03_010354) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "statuses", "users"
 end
